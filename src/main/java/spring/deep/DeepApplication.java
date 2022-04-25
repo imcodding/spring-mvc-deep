41ab8eb11a01cc2ae40e87ebfe511221c6d8f779
mia <55366664+imcodding@users.noreply.github.com>
2022-04-25 16:11:31 +0900

    메세지 적용

--- src/main/java/spring/deep/DeepApplication.java
+++ src/main/java/spring/deep/DeepApplication.java
@@ -10,4 +10,13 @@ public class DeepApplication {
 		SpringApplication.run(DeepApplication.class, args);
 	}
 
+	/* 스프링 부트는 MessageSource 를 자동으로 스프링 빈으로 등록
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasenames("messages", "errors");
+		messageSource.setDefaultEncoding("utf-8");
+		return messageSource;
+	}
+	 */
 }
