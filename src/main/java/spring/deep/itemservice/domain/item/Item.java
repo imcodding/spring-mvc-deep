16343d5b502868449f1f17943d5c57f65f71e1fd
imcodding <suy0226@naver.com>
2022-05-05 21:07:25 +0900

    Bean Validation - groups
    - 복잡도 올라감. 잘 사용하지 않는 방식
    => 주로 등록용 폼 객체와 수정용 폼 객체를 분리해서 사용

--- src/main/java/spring/deep/itemservice/domain/item/Item.java
+++ src/main/java/spring/deep/itemservice/domain/item/Item.java
@@ -10,21 +10,22 @@ import javax.validation.constraints.NotNull;
 @Data
 public class Item {
 
+    @NotNull(groups = UpdateCheck.class) // 수정 요구사항 추가
     private Long id;
 
-    @NotBlank
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
     /*
      문자 입력 시 타입 변환 실패
      => BeanValidation 적용 X, typeMismatch Field Error 적용
      */
-    @NotNull
-    @Range(min = 1000, max = 1000000)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull
-    @Max(9999)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = SaveCheck.class) // 수정요구사항 추가
     private Integer quantity;
 
     public Item() {
---
+++ src/main/java/spring/deep/itemservice/domain/item/UpdateCheck.java
@@ -0,0 +1,4 @@
+package spring.deep.itemservice.domain.item;
+
+public interface UpdateCheck {
+}
