6c339cbd9b88ce40338fa567461898fa7e3fd5bd
sooyun <55366664+imcodding@users.noreply.github.com>
2022-05-09 09:10:20 +0900

    ArgumentResolver 사용
    - 공통 작업이 필요할 때 컨트롤러를 더욱 편리하게 사용 가능하다.

--- src/main/java/spring/deep/login/WebConfig.java
+++ src/main/java/spring/deep/login/WebConfig.java
@@ -3,17 +3,25 @@ package spring.deep.login;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import spring.deep.login.web.argumentResolver.LoginMemberArgumentResolver;
 import spring.deep.login.web.filter.LogFilter;
 import spring.deep.login.web.filter.LoginCheckFilter;
 import spring.deep.login.web.interceptor.LogInterceptor;
 import spring.deep.login.web.interceptor.LoginInterceptor;
 
 import javax.servlet.Filter;
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
--- src/main/java/spring/deep/login/web/HomeController.java
+++ src/main/java/spring/deep/login/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import spring.deep.login.domain.member.Member;
 import spring.deep.login.domain.member.MemberRepository;
+import spring.deep.login.web.argumentResolver.Login;
 import spring.deep.login.web.session.SessionManager;
 
 import javax.servlet.http.HttpServletRequest;
@@ -75,7 +76,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/") //스프링 기능 제공. @SessionAttribute는 세션 생성 안 함.
+//    @GetMapping("/") //스프링 기능 제공. @SessionAttribute는 세션 생성 안 함.
     public String homeLoginV3Spring(
             @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
             Model model) {
@@ -89,4 +90,18 @@ public class HomeController {
         model.addAttribute("member", loginMember);
         return "loginHome";
     }
+
+    @GetMapping("/")
+    public String homeLoginV3ArgumentResolver(@Login Member loginMember, Model model) {
+
+        //세션에 회원 데이터가 없으면 home
+        if(loginMember == null) {
+            return "home";
+        }
+
+        //세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
 }
